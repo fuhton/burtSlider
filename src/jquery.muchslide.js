0c23d4a72be540c818bb91500c13d07a2ec61973
Marguerite Roth <marguerite@usetopscore.com>
2014-02-18 14:04:09 -0500

    Fixed vertical slide pagination

--- src/jquery.muchslide.js
+++ src/jquery.muchslide.js
@@ -217,10 +217,13 @@
             var self = this;
             self.selectedPage = selectedPage;
             self.selectedChild = self.$element.find("[data-counter=" + self.selectedPage + "]");
-            self.selectedChildLeft =  parseInt( self.selectedChild.css("left"), 10 );
-            self.selectedLeft = self.selectedChildLeft / self.sectionWidth;
-            if ( self.selectedChildLeft < 0 || self.selectedChildLeft > 0 ) {
-                self.moveEach( self.selectedChildLeft );
+            if ( self.settings.verticalSlide ) {
+		self.selectedDistance =  parseInt( self.selectedChild.css("top"), 10 );
+	    } else {
+		self.selectedDistance =  parseInt( self.selectedChild.css("left"), 10 );
+	    }
+            if ( self.selectedDistance < 0 || self.selectedDistance > 0 ) {
+                self.moveEach( self.selectedDistance );
             }
         },
         createPaged: function () {
